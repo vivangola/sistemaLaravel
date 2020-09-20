3c6e8c91de142377f1d66037441341a8a30b6c54
Denilson Perez Junior <42591811+vivangola@users.noreply.github.com>
2020-09-20 18:32:08 -0300

    Layout definido

---
+++ resources/views/templates/footer.blade.php
@@ -0,0 +1,8 @@
+<!-- Navbar-->
+<footer class="app-footer">
+    <div class="row">
+        <div class="col-md-12 text-center">
+            <p>FOOTER</p>
+        </div>
+    </div>
+</footer>
--- resources/views/templates/template.blade.php
+++ resources/views/templates/template.blade.php
@@ -1,185 +1,117 @@
 <!DOCTYPE html>
 <html lang="pt-br">
-  <head>
-    <meta name="description" content="">
-    <!-- Twitter meta-->
-    <meta property="twitter:card" content="">
-    <meta property="twitter:site" content="">
-    <meta property="twitter:creator" content="">
-    <!-- Open Graph Meta-->
-    <meta property="og:type" content="website">
-    <meta property="og:site_name" content="">
-    <meta property="og:title" content="">
-    <meta property="og:url" content="">
-    <meta property="og:image" content="">
-    <meta property="og:description" content="">
-    <title>@yield('title') - Sysvangola</title>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <head>
+        <meta name="description" content="">
 
-    <!-- Main CSS-->
-    <link rel="stylesheet" type="text/css" href="{{ url('assets/css/main.css') }}">
-    <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-    
-  </head>
-  <body class="app sidebar-mini">
-      
-    <script type="text/javascript" src="{{ url('assets/js/plugins/sweetalert.min.js') }}"></script>
-    <script src="{{ url('assets/js/jquery-3.3.1.min.js') }}"></script>
+        <!-- Twitter meta-->
+        <meta property="twitter:card" content="">
+        <meta property="twitter:site" content="">
+        <meta property="twitter:creator" content="">
+        <!-- Open Graph Meta-->
+        <meta property="og:type" content="website">
+        <meta property="og:site_name" content="">
+        <meta property="og:title" content="">
+        <meta property="og:url" content="">
+        <meta property="og:image" content="">
+        <meta property="og:description" content="">
+        <title>@yield('title') - Sysvangola</title>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <!-- Navbar-->
-    <header class="app-header"><a class="app-header__logo" href="/">Sysvangola</a>
-      <a class="app-sidebar__toggle" href="#" data-toggle="sidebar" aria-label="Hide Sidebar"></a>
-      <!-- Navbar Right Menu-->
-      <ul class="app-nav">
-        <li class="app-search">
-          <input class="app-search__input" type="search" placeholder="Pesquisar">
-          <button class="app-search__button"><i class="fa fa-search"></i></button>
-        </li>
-        <!--Notification Menu-->
-        <li class="dropdown"><a class="app-nav__item" href="#" data-toggle="dropdown" aria-label="Show notifications"><i class="fa fa-bell-o fa-lg"></i></a>
-          <ul class="app-notification dropdown-menu dropdown-menu-right">
-            <li class="app-notification__title">You have 4 new notifications.</li>
-            <div class="app-notification__content">
-              <li><a class="app-notification__item" href="javascript:;"><span class="app-notification__icon"><span class="fa-stack fa-lg"><i class="fa fa-circle fa-stack-2x text-primary"></i><i class="fa fa-envelope fa-stack-1x fa-inverse"></i></span></span>
-                  <div>
-                    <p class="app-notification__message">Lisa sent you a mail</p>
-                    <p class="app-notification__meta">2 min ago</p>
-                  </div></a></li>
-              <li><a class="app-notification__item" href="javascript:;"><span class="app-notification__icon"><span class="fa-stack fa-lg"><i class="fa fa-circle fa-stack-2x text-danger"></i><i class="fa fa-hdd-o fa-stack-1x fa-inverse"></i></span></span>
-                  <div>
-                    <p class="app-notification__message">Mail server not working</p>
-                    <p class="app-notification__meta">5 min ago</p>
-                  </div></a></li>
-              <li><a class="app-notification__item" href="javascript:;"><span class="app-notification__icon"><span class="fa-stack fa-lg"><i class="fa fa-circle fa-stack-2x text-success"></i><i class="fa fa-money fa-stack-1x fa-inverse"></i></span></span>
-                  <div>
-                    <p class="app-notification__message">Transaction complete</p>
-                    <p class="app-notification__meta">2 days ago</p>
-                  </div></a></li>
-              <div class="app-notification__content">
-                <li><a class="app-notification__item" href="javascript:;"><span class="app-notification__icon"><span class="fa-stack fa-lg"><i class="fa fa-circle fa-stack-2x text-primary"></i><i class="fa fa-envelope fa-stack-1x fa-inverse"></i></span></span>
-                    <div>
-                      <p class="app-notification__message">Lisa sent you a mail</p>
-                      <p class="app-notification__meta">2 min ago</p>
-                    </div></a></li>
-                <li><a class="app-notification__item" href="javascript:;"><span class="app-notification__icon"><span class="fa-stack fa-lg"><i class="fa fa-circle fa-stack-2x text-danger"></i><i class="fa fa-hdd-o fa-stack-1x fa-inverse"></i></span></span>
-                    <div>
-                      <p class="app-notification__message">Mail server not working</p>
-                      <p class="app-notification__meta">5 min ago</p>
-                    </div></a></li>
-                <li><a class="app-notification__item" href="javascript:;"><span class="app-notification__icon"><span class="fa-stack fa-lg"><i class="fa fa-circle fa-stack-2x text-success"></i><i class="fa fa-money fa-stack-1x fa-inverse"></i></span></span>
-                    <div>
-                      <p class="app-notification__message">Transaction complete</p>
-                      <p class="app-notification__meta">2 days ago</p>
-                    </div></a></li>
-              </div>
-            </div>
-            <li class="app-notification__footer"><a href="#">See all notifications.</a></li>
-          </ul>
-        </li>
-        <!-- User Menu-->
-        <li class="dropdown"><a class="app-nav__item" href="#" data-toggle="dropdown" aria-label="Open Profile Menu"><i class="fa fa-user fa-lg"></i></a>
-          <ul class="dropdown-menu settings-menu dropdown-menu-right">
-            <!--li><a class="dropdown-item" href="page-user.html"><i class="fa fa-cog fa-lg"></i> Settings</a></li-->
-            <!--li><a class="dropdown-item" href="page-user.html"><i class="fa fa-user fa-lg"></i> Profile</a></li-->
-            <li><a class="dropdown-item" href="{{ url('/logout') }}"><i class="fa fa-sign-out fa-lg"></i> Sair </a></li>
-          </ul>
-        </li>
-      </ul>
-    </header>
-    <!-- Sidebar menu-->
-    <div class="app-sidebar__overlay" data-toggle="sidebar"></div>
-      <aside class="app-sidebar">
-        <div class="app-sidebar__user">
-          <img class="app-sidebar__user-avatar" src="https://s3.amazonaws.com/uifaces/faces/twitter/jsa/48.jpg" alt="User Image">
-          <div>
-            <p class="app-sidebar__user-name">John Doe</p>
-            <p class="app-sidebar__user-designation">Frontend Developer</p>
-          </div>
-        </div>
-        <ul class="app-menu">
-          <li><a class="app-menu__item active" href="/"><i class="app-menu__icon fa fa-home"></i><span class="app-menu__label">Inicio</span></a></li>
-          <li class="treeview"><a class="app-menu__item" href="#" data-toggle="treeview"><i class="app-menu__icon fa fa-file-text"></i><span class="app-menu__label">Cadastros</span><i class="treeview-indicator fa fa-angle-right"></i></a>
-            <ul class="treeview-menu">
-              <li><a class="treeview-item" href="/funcionarios"><i class="icon fa fa-circle-o"></i>Funcionários</a></li>
-              <li><a class="treeview-item" href="/materiais"><i class="icon fa fa-circle-o"></i>Materiais</a></li>
-              <li><a class="treeview-item" href="/planos"><i class="icon fa fa-circle-o"></i>Planos</a></li>
-              <li><a class="treeview-item" href="/usuarios"><i class="icon fa fa-circle-o"></i>Usuários</a></li>
-            </ul>
-          </li>
-          <!--li class="treeview"><a class="app-menu__item" href="#" data-toggle="treeview"><i class="app-menu__icon fa fa-laptop"></i><span class="app-menu__label">UI Elements</span><i class="treeview-indicator fa fa-angle-right"></i></a>
-            <ul class="treeview-menu">
-              <li><a class="treeview-item" href="bootstrap-components.html"><i class="icon fa fa-circle-o"></i> Bootstrap Elements</a></li>
-              <li><a class="treeview-item" href="https://fontawesome.com/v4.7.0/icons/" target="_blank" rel="noopener"><i class="icon fa fa-circle-o"></i> Font Icons</a></li>
-              <li><a class="treeview-item" href="ui-cards.html"><i class="icon fa fa-circle-o"></i> Cards</a></li>
-              <li><a class="treeview-item" href="widgets.html"><i class="icon fa fa-circle-o"></i> Widgets</a></li>
-            </ul>
-          </li>
-          <li><a class="app-menu__item" href="charts.html"><i class="app-menu__icon fa fa-pie-chart"></i><span class="app-menu__label">Charts</span></a></li>
-          <li class="treeview"><a class="app-menu__item" href="#" data-toggle="treeview"><i class="app-menu__icon fa fa-edit"></i><span class="app-menu__label">Forms</span><i class="treeview-indicator fa fa-angle-right"></i></a>
-            <ul class="treeview-menu">
-              <li><a class="treeview-item" href="form-components.html"><i class="icon fa fa-circle-o"></i> Form Components</a></li>
-              <li><a class="treeview-item" href="form-custom.html"><i class="icon fa fa-circle-o"></i> Custom Components</a></li>
-              <li><a class="treeview-item" href="form-samples.html"><i class="icon fa fa-circle-o"></i> Form Samples</a></li>
-              <li><a class="treeview-item" href="form-notifications.html"><i class="icon fa fa-circle-o"></i> Form Notifications</a></li>
-            </ul>
-          </li>
-          <li class="treeview"><a class="app-menu__item" href="#" data-toggle="treeview"><i class="app-menu__icon fa fa-th-list"></i><span class="app-menu__label">Tables</span><i class="treeview-indicator fa fa-angle-right"></i></a>
-            <ul class="treeview-menu">
-              <li><a class="treeview-item" href="table-basic.html"><i class="icon fa fa-circle-o"></i> Basic Tables</a></li>
-              <li><a class="treeview-item" href="table-data-table.html"><i class="icon fa fa-circle-o"></i> Data Tables</a></li>
-            </ul>
-          </li>
-          <li class="treeview"><a class="app-menu__item" href="#" data-toggle="treeview"><i class="app-menu__icon fa fa-file-text"></i><span class="app-menu__label">Pages</span><i class="treeview-indicator fa fa-angle-right"></i></a>
-            <ul class="treeview-menu">
-              <li><a class="treeview-item" href="blank-page.html"><i class="icon fa fa-circle-o"></i> Blank Page</a></li>
-              <li><a class="treeview-item" href="page-login.html"><i class="icon fa fa-circle-o"></i> Login Page</a></li>
-              <li><a class="treeview-item" href="page-lockscreen.html"><i class="icon fa fa-circle-o"></i> Lockscreen Page</a></li>
-              <li><a class="treeview-item" href="page-user.html"><i class="icon fa fa-circle-o"></i> User Page</a></li>
-              <li><a class="treeview-item" href="page-invoice.html"><i class="icon fa fa-circle-o"></i> Invoice Page</a></li>
-              <li><a class="treeview-item" href="page-calendar.html"><i class="icon fa fa-circle-o"></i> Calendar Page</a></li>
-              <li><a class="treeview-item" href="page-mailbox.html"><i class="icon fa fa-circle-o"></i> Mailbox</a></li>
-              <li><a class="treeview-item" href="page-error.html"><i class="icon fa fa-circle-o"></i> Error Page</a></li>
-            </ul>
-          </li>
-          <li><a class="app-menu__item" href="docs.html"><i class="app-menu__icon fa fa-file-code-o"></i><span class="app-menu__label">Docs</span></a></li-->
-        </ul>
-      </aside>
-  </div>
-    <!-- Main Content -->
-    <main class="app-content">        
-        <div class="app-title">
-            <div>
-                <h1>
-                    <h1><i class="@yield('icone')"></i>&nbsp;&nbsp;@yield('titulo')
-                </h1>
-            </div>
-            <ul class="app-breadcrumb breadcrumb">
-            <li class="breadcrumb-item"></li>
-            </ul>
-        </div>
-        @yield('content')
-    </main>
-
-    <!-- Essential javascripts for application to work-->
-    <script src="{{ url('assets/js/popper.min.js') }}"></script>
-    <script src="{{ url('assets/js/bootstrap.min.js') }}"></script>
-    <script src="{{ url('assets/js/main.js') }}"></script>
-    <script type="text/javascript" src="{{ url('assets/js/plugins/bootstrap-notify.min.js') }}"></script>
-    <script src="{{ url('assets/js/plugins/pace.min.js') }}"></script>
-    <script src="{{ url('assets/js/script.js' )}}"></script>
+        <!-- Main CSS-->
+        <link rel="stylesheet" type="text/css" href="{{ url('assets/css/main.css') }}">
+        <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    </head>
+    <body class="app sidebar-mini">
         
-    <!-- Google analytics script-->
-    <script type="text/javascript">
-      if(document.location.hostname == 'pratikborsadiya.in') {
-      	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-      	(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-      	m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-      	})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-      	ga('create', 'UA-72504830-1', 'auto');
-      	ga('send', 'pageview');
-      }
-    </script>
-  </body>
+        <script type="text/javascript" src="{{ url('assets/js/plugins/sweetalert.min.js') }}"></script>
+        <script type="text/javascript" src="{{ url('assets/js/jquery-3.3.1.min.js') }}"></script>
+        <script type="text/javascript" src="{{ url('assets/js/plugins/chart.js') }}" ></script>
+
+        <!-- Header-->
+        @include('templates.header')
+
+        <!-- Sidebar menu-->
+        @include('templates.sidebar')
+
+        <!-- Main Content -->
+        <main class="app-content">        
+            <div class="app-title">
+                <div>
+                    <h1>
+                        <h1><i class="@yield('icone')"></i>&nbsp;&nbsp;@yield('titulo')
+                    </h1>
+                </div>
+                <ul class="app-breadcrumb breadcrumb">
+                <li class="breadcrumb-item"></li>
+                </ul>
+            </div>
+            @yield('content')
+        </main>
+
+        <!-- Footer -->
+        @include('templates.footer')
+
+        <!-- Essential javascripts for application to work-->
+        <script src="{{ url('assets/js/popper.min.js') }}"></script>
+        <script src="{{ url('assets/js/bootstrap.min.js') }}"></script>
+        <script src="{{ url('assets/js/main.js') }}"></script>
+        <script type="text/javascript" src="{{ url('assets/js/plugins/bootstrap-notify.min.js') }}"></script>
+        <script src="{{ url('assets/js/plugins/pace.min.js') }}"></script>
+        <script src="{{ url('assets/js/script.js' )}}"></script>
+            
+        <!-- Google analytics script-->
+        <script type="text/javascript">
+            if(document.location.hostname == 'pratikborsadiya.in') {
+                (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+                (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+                m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+                })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+                ga('create', 'UA-72504830-1', 'auto');
+                ga('send', 'pageview');
+            }
+        </script>
+
+        <!-- Charts -->
+        <script type="text/javascript">
+            var data = {
+                labels: ["Janeiro", "Feveiro", "Março", "Abril", "Maio","Junho", "Julho", "Agosto", "Setembro"],
+                datasets: [
+                    {
+                        label: "My First dataset",
+                        fillColor: "rgba(151,187,205,0.2)",
+                        strokeColor: "rgba(151,187,205,1)",
+                        pointColor: "rgba(151,187,205,1)",
+                        pointStrokeColor: "#fff",
+                        pointHighlightFill: "#fff",
+                        pointHighlightStroke: "rgba(151,187,205,1)",
+                        data: [18000,16200,16700,17500,17000,18000,21000,24000,23000]
+                    }
+                ]
+            };
+            var ctxb = $("#barChartDemo").get(0).getContext("2d");
+            var barChart = new Chart(ctxb).Bar(data);
+            var data2 = {
+                labels: ["Janeiro", "Feveiro", "Março", "Abril", "Maio","Junho", "Julho", "Agosto", "Setembro"],
+                datasets: [
+                    {
+                        label: "My First dataset",
+                        fillColor: "rgba(0,150,136,0.2)",
+                        strokeColor: "rgba(0,150,136,1)",
+                        pointColor: "rgba(0,150,136,1)",
+                        pointStrokeColor: "#fff",
+                        pointHighlightFill: "#fff",
+                        pointHighlightStroke: "rgba(0,150,136,1)",
+                        data: [28, 48, 40, 19, 26, 28, 48, 40, 19]
+                    }
+                ]
+            };
+            var ctxb2 = $("#barChartDemo2").get(0).getContext("2d");
+            var barChart2 = new Chart(ctxb2).Bar(data2);
+        </script>
+
+
+    </body>
+
 </html>
