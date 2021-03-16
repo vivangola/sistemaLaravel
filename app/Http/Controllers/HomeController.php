8bd99db6d8c41d7000916766a6d661aa312d834e
vivangola <denilson.junior@crmall.com>
2021-03-16 02:35:53 -0300

    Contador Obito da Home

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,21 +4,25 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\ContaModel;
+use App\Models\ObitoModel;
 use Auth;
+
 class HomeController extends Controller
 {
 
     private $contas;
+    private $obitos;
 
     public function __construct(){
         $this->contas = new ContaModel;
+        $this->obitos = new ObitoModel;
     }
 
     public function index(){
         $contas = $this->contas->count();
         $ativas = $this->contas->where('tipo_status_id',1)->count();
         $debito = $this->contas->where('tipo_status_id',2)->count();
-        $obitos = 0;
+        $obitos = $this->obitos->count();
         return view('home',compact('contas','ativas','debito','obitos'));
     }
 }
